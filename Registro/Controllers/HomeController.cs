018c9ad4af1e84f805cd5b19bf552e1f24b4c829
Daniel Manzano <daniel.manzanoe@edu.uah.es>
2018-11-07 12:59:43 +0100

    Subida inicial de la nueva versión del proyecto

---
+++ Registro/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Registro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your index page.";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult AddOrEdit()
+        {
+            ViewBag.Message = "Your registration page.";
+
+            return View();
+        }
+    }
+}
---
+++ Registro/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Registro;
+
+namespace Registro.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            Usuario usuarioModel = new Usuario();
+            return View(usuarioModel);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Usuario userModel)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                if(dbModel.Usuario.Any( x => x.correo == userModel.correo))
+                {
+                    ViewBag.DuplicateMessage ="El usuario ya existe";
+                    return View("AddOrEdit", userModel);
+                }
+                dbModel.Usuario.Add(userModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccesMessage = "Registro completado!";
+            return (View("AddOrEdit", new Usuario()));
+        }
+    }
+}
